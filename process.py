74cad460712cf9b586f5224a5c53b3d09becd59e
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-05-20 14:49:49 +0530

    added savitsky golay function

--- process.py
+++ process.py
@@ -6,7 +6,8 @@
 # This log will then be used in a good ol' 3x3 matrix system of linear equations and will then be solved for the concentrations.
 # Here, the epsilons will be entered as variables RIGHT at the start so that they can be edited as required (and the inverse found each time on-the-fly
 
-# THe smoothing function that is being used.. taken from http://wiki.scipy.org/Cookbook/SignalSmooth
+# THe smoothing functions that is being used.. 
+# the first 'smooth' is taken from http://wiki.scipy.org/Cookbook/SignalSmooth
 # skip this function to see the main code...
 def smooth(x,window_len=11,window='hanning'):
     """smooth the data using a window with requested size.
@@ -65,6 +66,76 @@ def smooth(x,window_len=11,window='hanning'):
     y=numpy.convolve(w/w.sum(),s,mode='valid')
     return y
 
+# the next one models a savitzky-golay smoothening, taken from https://gist.github.com/RyanHope/2321077
+def savitzky_golay( y, window_size, order, deriv = 0 ):
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techhniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+    Examples
+    --------
+    t = np.linspace(-4, 4, 500)
+    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
+    ysg = savitzky_golay(y, window_size=31, order=4)
+    import matplotlib.pyplot as plt
+    plt.plot(t, y, label='Noisy signal')
+    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
+    plt.plot(t, ysg, 'r', label='Filtered signal')
+    plt.legend()
+    plt.show()
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
+    try:
+        window_size = np.abs( np.int( window_size ) )
+        order = np.abs( np.int( order ) )
+    except ValueError, msg:
+        raise ValueError( "window_size and order have to be of type int" )
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError( "window_size size must be a positive odd number" )
+    if window_size < order + 2:
+        raise TypeError( "window_size is too small for the polynomials order" )
+    order_range = range( order + 1 )
+    half_window = ( window_size - 1 ) // 2
+    # precompute coefficients
+    b = np.mat( [[k ** i for i in order_range] for k in range( -half_window, half_window + 1 )] )
+    m = np.linalg.pinv( b ).A[deriv]
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window + 1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs( y[-half_window - 1:-1][::-1] - y[-1] )
+    y = np.concatenate( ( firstvals, y, lastvals ) )
+    return np.convolve( m, y, mode = 'valid' )
+
 # there are three epsilon arrays, and they in turn shall have 3 members,
 # the exctinction coefficients at 630, 850, 950 respectively.
 epsilon_Hb = []
@@ -103,21 +174,27 @@ b, a = scipy.signal.butter(butterOrder, [fCutoffHigh/fnyq])
 filteredAmpl = scipy.signal.lfilter(b, a, amplitude)
 
 # then we totally smooth the shit out of the filtered signal
-smoothingOrder = 501		# this needs to be subtracted later from the smoothed array
-smoothedAmpl = smooth(filteredAmpl, smoothingOrder)
-print len(smoothedAmpl)
-print len(time)
+# smoothingOrder = 501		# this needs to be subtracted later from the smoothed array
+# smoothedAmpl = smooth(filteredAmpl, filteredAmpl)
 
-# we then plot these two w.r.t. time
+# Right. After this we bandpass it, while letter DC go through but not allowing a low-freqyency region between 0 and 0.5Hz to go through
+b,a = scipy.signal.butter(1, [0.1/fnyq, 0.5/fnyq], btype='bandstop')
+# b = scipy.signal.firwin(2, cutoff=0.1, nyq=fnyq, pass_zero=True)
+lowFreqRemovedAmpl = scipy.signal.lfilter(b, a, filteredAmpl)
+
+# we then plot these w.r.t. time
 plt.figure(1)
 plt.clf
 plt.autoscale(enable=True, axis='both', tight=True)
 plt.plot(time, amplitude, 'g', label='Original signal (Hz)')		# original, green
 plt.plot(time, filteredAmpl, 'b', label='Filtered signal (Hz)')		# filtered
-plt.plot(time, smoothedAmpl[(smoothingOrder/2)-1:-(smoothingOrder/2)-1], 'k', label='Filtered signal (Hz)')		# smoothed, black colour 'k' and phase-adjusted to match the main signals
+# plt.plot(time, smoothedAmpl[(smoothingOrder/2)-1:-(smoothingOrder/2)-1], 'k', label='Filtered signal (Hz)')		# smoothed, black colour 'k' and phase-adjusted to match the main signals
+plt.plot(time, lowFreqRemovedAmpl, 'r', label='Leveled signal (Hz)')
 
 plt.xlabel('time (seconds)')
 plt.grid(True)
 plt.legend(loc='upper left')
 
 plt.show()
+
+# next what we want to do is to be able to remove the low-freqyency feature
