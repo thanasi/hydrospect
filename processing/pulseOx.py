0432a11006da4b1372d1fe9964244bb824e8ddb3
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-06-02 18:44:33 +0530

    templated the python pulseOx file, now will fill in the gaps

--- processing/pulseOx.py
+++ processing/pulseOx.py
@@ -3,3 +3,23 @@
 # data will be taken for a fixed period of time from the teensy after the LED is turned on, and then stopped.
 # In that meantime it shall be processed and the numbers (pulseOx and heart rate) shall be thrown out.
 
+##### IMPORT STATEMENTS, VARIABLE DECLARATIONS..
+import serial, numpy
+
+# step1: open the serial port
+
+teensy = serial.Serial(3, 115200)		# open serial port
+
+# step 2: send the "begin the red colour" command...
+
+# step 3: start acquiring data and storing it into a numpy array
+
+# step 4: After a certain number of datapoints have been acquired, stop acqisition, send the "begin the IR colour" command...
+
+# step 5: start acquiring data and storing it into a second variable/array
+
+# step 6: PROCESSING begins. Find the peaks for both variables. Store into variable/array. Find the time difference between peaks, and store this into variables.
+
+# step 7: find the logs of the ratios from the peaks.
+
+# step 8: find the o2 sat value yo. spit it out.
